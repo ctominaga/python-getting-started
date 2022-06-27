ad525f721ebb61416a40e586d09af461c67cabed
ctominaga <christophertominaga@hotmail.com>
2022-06-27 13:43:09 -0300

    Demo

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -7,9 +9,8 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = requests.get('https://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    times = int(os.environ.get('TIMES', 3))
+    return HttpResponse('Hello! ' * times)
 
 
 def db(request):
